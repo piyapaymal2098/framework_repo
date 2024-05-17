3709d3540c17f7ad5705c9791c94701482159375
anil1 <anil1@spyder>
2024-05-17 13:32:52 +0530

    new project

---
+++ seleniumS/src/test/java/pac1/contactTest.java
@@ -0,0 +1,22 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class contactTest {
+@Test
+public void creatcontactTest() {
+	String URL = System.getProperty("url");
+	String BROWSER = System.getProperty("browser");
+	String USERNAME = System.getProperty("username");
+ String PASSWORD = System.getProperty("password");
+System.out.println("execute creatcontactTest");
+System.out.println(URL);
+System.out.println(BROWSER);
+System.out.println(USERNAME);
+System.out.println(PASSWORD);
+}
+@Test
+public void modifyconntactTest() {
+System.out.println("execute modifyconntactTest");
+}
+}
---
+++ seleniumS/src/test/java/pac2/orgTest.java
@@ -0,0 +1,14 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+@Test
+public void creatorgTest() {
+System.out.println("execute creatorgTest");
+}
+@Test
+public void modifyorgTest() {
+System.out.println("execute modifyorgTest");
+}
+}
