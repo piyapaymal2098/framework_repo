4dd04fc1f069f4bd84bcee9e8f7153257b0565f6
anil1 <anil1@spyder>
2024-05-18 10:43:21 +0530

    comit

--- seleniumS/src/test/java/pac2/orgTest.java
+++ seleniumS/src/test/java/pac2/orgTest.java
@@ -6,6 +6,17 @@ public class orgTest {
 @Test
 public void creatorgTest() {
 System.out.println("execute creatorgTest");
+String URL = System.getProperty("url");
+String BROWSER = System.getProperty("browser");
+String USERNAME = System.getProperty("username");
+String PASSWORD = System.getProperty("password");
+System.out.println("execute creatcontactTest");
+System.out.println(URL);
+System.out.println(BROWSER);
+System.out.println(USERNAME);
+System.out.println(PASSWORD);
+
+
 }
 @Test
 public void modifyorgTest() {
